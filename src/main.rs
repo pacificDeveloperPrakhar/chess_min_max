5e171d0583f36490b682cbabc44171f2b5d7df6c
prakhar vishwakarma <prakharvision@gmail.com>
2025-11-13 11:52:58 +0000

    also implemented the move generation function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 pub mod data_structures;
 pub mod operations;
 use data_structures::bitboard::*;
+use operations::*;
 use operations::moves_generate_pieces::*;
 use operations::piece_wise_generation::*;
 fn main() {
-   let ffn=String::from("rn1k1b1r/ppp2ppp/4p3/1Q6/8/8/PPPPP1PP/RNB1K1NR w KQkq - 0 1");
+   let ffn=String::from("rnbqkb1r/ppp2ppp/4p3/3p4/1Q6/3P1N1P/PPP1PnP1/RNB1KB1R w KQkq - 0 1");
    let bitboards=operations::init_board(&ffn);
    // let moves=pawn_moves(bitboards,1,2);
    // let moves=operations::display_bitboard(moves);
    // println!("{}",moves);
    // println!("{}",bitboards[PieceColor::W as usize][Piece::P as usize]);
    // println!("{}",bitboards[PieceColor::B as usize][Piece::P as usize]);
-   let checking=safety_move_one_square_move(bitboards,0,3);
+   let checking=generate_moves(bitboards,0,5);
    // let moves=operations::display_bitboard(bitboards[PieceColor::W as usize][Piece::A as usize]);
    let moves=operations::display_bitboard(checking);
    println!("{}",moves);
--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -2,6 +2,7 @@ pub mod moves_generate_pieces;
 pub mod piece_wise_generation;
 use std::vec::Vec;
 use crate::data_structures::bitboard::PieceColor;
+use crate::operations::piece_wise_generation::*;
 use crate::data_structures::bitboard::Piece;
 pub fn init_board(epd:&String)->[[u64;7];2]
 {
@@ -75,7 +76,33 @@ pub fn display_bitboard(board: u64) -> String {
 }
 
 
-pub fn generate_moves(bitboards:[[u64;7];2],rank:usize,file:usize)->(u64,u64)
+pub fn generate_moves(bitboards:[[u64;7];2],rank:usize,file:usize)->u64
 {
- return (0,0);
+ let piece_position:u64=1<<((rank*8) as u64 +file as u64);
+ let mut piece_color:usize=2;
+ let mut piece:usize=6;
+//  now get the piece type first and the color of it 
+ for i in 0..2
+ {
+  for j in 0..6
+  {
+   if (bitboards[i][j]& piece_position) !=0
+   {
+    piece_color=i;
+    piece=j;
+    break;
+   }
+  }
+ }
+
+ match piece
+ {
+  0 => safety_move_one_square_move(bitboards,rank,file),
+  1 =>piece_wise_l_squares(bitboards,rank,file),
+  2=>diagnol_move_generation_with_king_safety(bitboards,rank,file),
+  3=>piece_wise_horizontal_vertical_moves(bitboards,rank,file),
+  4=>safety_move_one_square_move(bitboards,rank,file),
+  5=>diagnol_move_generation_with_king_safety(bitboards,rank,file)|piece_wise_horizontal_vertical_moves(bitboards,rank,file),
+  _=>0 as u64
+ }
 }
